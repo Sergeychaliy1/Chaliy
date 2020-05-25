28daed4ebc08a871ec820b5bd019cac8db1a7c52
SergeyChaliy <60869708+Sergeychaliy1@users.noreply.github.com>
2020-05-25 10:15:50 +0300

    Update MyString .h

--- строки/MyString .h
+++ строки/MyString .h
@@ -1,8 +1,8 @@
+#pragma once
+
 unsigned int strLen(const char *str);
 void strCpy(char *dest, const char *source);
-int strnCpy(char *dest, const char *source, unsigned int len);
 void strCat(char *dest, const char *source);
-void strnCat(char *dest, const char *source, unsigned int len);
 void strReverse(char *str);
 void strToUpper(char *str);
 void strToLower(char *str);

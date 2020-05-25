b3b80162cfa89ece4b514c6a14527c1af9c2be4a
SergeyChaliy <60869708+Sergeychaliy1@users.noreply.github.com>
2020-05-25 10:38:15 +0300

    Update main.cpp

--- строки/main.cpp
+++ строки/main.cpp
@@ -1,24 +1,44 @@
-#include "MyString.h"
 #include <iostream>
+#include "MyString.h"
+
 using namespace std;
 
 int main()
 {
-	      setlocale(LC_ALL, "Russian");
 	      char str[30] ="Happy NeW year 2020";
-}
-  {
-    cout<<"     функция 9 - возвращает индекс первого вхождения подстроки substr в str"<<endl<<endl;
+	
+	
+	//#1
+	
 	char strstr[20]="123qwerty4444" ;
 	char substr[20]="qwerty" ;
+	
+	cout << strstr << endl;
+	cout << substr << endl;
 	cout<<strstr<<endl<<substr<<endl;
 	cout<<strStr(strstr, substr)<<endl<<endl;
 
-	cout<<"     функция10 - 0(если строки равны), 1(если первая больше), -1(если вторая больше)"<<endl<<endl;		  
+	//#2
+	
 	char str1[30] ="5555a55";
 	char str2[30] ="5555b55";
-	cout<<str1<<endl<<str2<<endl;
+cout << str1 << endl;
+	cout << str2 << endl;
 	cout<<strCmp(str1,str2)<<endl<<endl;
+	
+	
+	
+	//#3
+	
+	char strLower[] = "sdjkfhsdkjlf KSLDJHFKJLSDHF dkjfhskjfh 12345678910";
+	char strUpper[] = "FJKHSDKLJFHSLDK sfhksdfh SDKLFJHSKDJHF 12345678910";
+
+	strToUpper(strLower);
+	strToLower(strUpper);
+
+	cout << strLower << endl;
+	cout << strUpper << endl;
+
 	
 	system("pause");
 	return 0;

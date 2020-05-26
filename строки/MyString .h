c22c2fb9c6c84696cf27e093e36f112b017c0168
SergeyChaliy <60869708+Sergeychaliy1@users.noreply.github.com>
2020-05-26 11:38:43 +0300

    Update MyString .h

--- строки/MyString .h
+++ строки/MyString .h
@@ -8,3 +8,4 @@ void strToUpper(char *str);
 void strToLower(char *str);
 int strStr(const char *substr,const char *str);
 int strCmp(const char *str1,const char *str2);
+char* deleteTheLetter(char* text, char letter)

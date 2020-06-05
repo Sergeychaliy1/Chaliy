f0b769711f8f32ce6409cb5f50159521c82443e6
SergeyChaliy <60869708+Sergeychaliy1@users.noreply.github.com>
2020-06-05 16:18:06 +0300

    Update ConsoleApplication2.cpp

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -2,9 +2,27 @@
 #include <Windows.h>
 #include <string>
 #include <conio.h>
+#include <stdlib.h>
 
 using namespace std;
-
+const int height = 25;
+const int width = 70;
+void Draw() {
+	for (int i = 0; i < width; i++)
+		cout << "#";
+	cout << endl;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			if (j == 0 || j == width-2)
+				cout << "#";
+			cout << " ";
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < width; i++)
+		cout << "#";
+	cout << endl;
+}
 enum Button
 {
 	ARROW_DOWN = 72,
@@ -17,16 +35,16 @@ int Key(int cursore, bool& menu)
 	switch (_getch())
 	{
 	case ARROW_DOWN:
-		if (cursore % 5 > 0)
-			return cursore - 1;
-		else
-			return cursore;
-		break;
-	case ARROW_UP:
-		if (cursore % 5 < 4)
+		if (cursore <4)
 			return cursore + 1;
 		else
-			return cursore;
+			return cursore;//не изменит положение
+		break;
+	case ARROW_UP:
+		if (cursore > 0 && cursore < 5)
+			return cursore - 1;
+		else
+			return cursore;//чтобы не шло выше
 		break;
 	case ENTER:
 		if (cursore < 5)
@@ -37,15 +55,10 @@ int Key(int cursore, bool& menu)
 				return 0;
 			}
 			else
-				return 5 * cursore + 5;
+				return  cursore + 5;
 		}
-		else if (cursore % 5 == 4)
-		{
-			system("cls");
-			return (cursore - cursore % 5) / 5 - 1;
-		}
-		else
-			return cursore;
+		else 
+			return cursore -5 ;
 		break;
 	default:
 		return cursore;
@@ -64,56 +77,50 @@ int main()
 	bool menu = true;
 	setlocale(LC_ALL, "Rus");
 	int cursore = 0;
-	string array[25];
+	string array[10];
 	array[0] = "Пункт 1";
 	array[1] = "Пункт 2";
 	array[2] = "Пункт 3";
 	array[3] = "Пункт 4";
 	array[4] = "Выход";
 
-	array[5] = "Пункт 1.1";
-	array[6] = "Пункт 1.2";
-	array[7] = "Пункт 1.3";
-	array[8] = "Пункт 1.4";
-	array[9] = "Обратно к 1";
-
-	array[10] = "Пункт 2.1";
-	array[11] = "Пункт 2.2";
-	array[12] = "Пункт 2.3";
-	array[13] = "Пункт 2.4";
-	array[14] = "Обратно к 2";
-
-	array[15] = "Пункт 3.1";
-	array[16] = "Пункт 3.2";
-	array[17] = "Пункт 3.3";
-	array[18] = "Пункт 3.4";
-	array[19] = "Обратно к 3";
-
-	array[20] = "Пункт 4.1";
-	array[21] = "Пункт 4.2";
-	array[22] = "Пункт 4.3";
-	array[23] = "Пункт 4.4";
-	array[24] = "Обратно к 4";
-
-	SetConsoleTextAttribute(hCon, (WORD)((0 << 4) | 15));
+	array[5] = " Вы вошли в Пункт 1";
+	array[6] = " Вы вошли в Пункт 2";
+	array[7] = " Вы вошли в Пункт 3";
+	array[8] = " Вы вошли в Пункт 4";
+	array[9] = " Нажмите Enter чтобы выйти";
 
 	while (menu)
 	{
-		for (int i = cursore - cursore % 5; i < cursore - cursore % 5 + 5; i++)
+		system("cls");//очищается и переносится на новую строку
+		SetConsoleTextAttribute(hCon, (WORD)((0 << 4) | 12));
+		Draw();
+		SetConsoleTextAttribute(hCon, (WORD)((0 << 4) | 15));
+	if (cursore < 5)
+	{
+		for (int i = 0; i < 5; i++)
 		{
-			cPos.Y = 10 + i - cursore + cursore % 5;
-			SetConsoleCursorPosition(hCon, cPos);
-			if (i == cursore)
+			cPos.Y = 8 + i;
+			SetConsoleCursorPosition(hCon, cPos);//устанавливает курсор на поставленные координаты
+			if (i == cursore)//курсор==стротке
 			{
-				SetConsoleTextAttribute(hCon, (WORD)((0 << 4) | 7));
+				SetConsoleTextAttribute(hCon, (WORD)((0 << 4) | 12));//цвет до 
 				cout << array[i];
-				SetConsoleTextAttribute(hCon, (WORD)((0 << 4) | 15));
+				SetConsoleTextAttribute(hCon, (WORD)((0 << 4) | 15));//цвет после
 			}
 			else
 				cout << array[i];
 		}
-
+	}
+	else
+		{
+			cPos.Y = 10;
+			SetConsoleCursorPosition(hCon, cPos);
+			cout << array[cursore];//курсор указывает на пункт
+			cPos.Y = 12;
+			SetConsoleCursorPosition(hCon, cPos);
+			cout << array[9];
+		}
 		cursore = Key(cursore, menu);
 	}
 }
-

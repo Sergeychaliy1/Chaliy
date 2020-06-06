47acdc57b70cdce5c8b6c0faa82d35f73b00e579
SergeyChaliy <60869708+Sergeychaliy1@users.noreply.github.com>
2020-06-06 20:48:44 +0300

    Update MyString .h

--- строки/MyString .h
+++ строки/MyString .h
@@ -8,4 +8,6 @@ void strToUpper(char *str);
 void strToLower(char *str);
 int strStr(const char *substr,const char *str);
 int strCmp(const char *str1,const char *str2);
-char* deleteTheLetter(char* text, char letter)
+char* deleteTheLetter(char* text, char letter);
+  char* strToLowerDyn(const char* str);
+ char* StrReverseDyn(const char* str);

6d8e305e9f7f2098526a64e8e5dbbe2daa8be8a4
SergeyChaliy <60869708+Sergeychaliy1@users.noreply.github.com>
2020-06-11 21:22:09 +0300

    Update ConsoleApplication2.cpp

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 
 using namespace std;
-#define SIZE 5
 const int height = 25;
 const int width = 70;
 void Draw() {
@@ -31,41 +30,53 @@ enum Button
 	ENTER = 13
 };
 
-int Key(int cursore, bool& menu)
+int Key(int cursore, bool& menu, int SIZE)
 {
 	switch (_getch())
 	{
 	case ARROW_DOWN:
-		if (cursore < SIZE-1)
-			return cursore + 1;
+		if (cursore < 2*SIZE - 2 && !(cursore%2))
+			return cursore + 2;
 		else
 			return cursore;//не изменит положение
 		break;
 	case ARROW_UP:
-		if (cursore > 0 && cursore < SIZE)
-			return cursore - 1;
+		if (cursore > 0 && !(cursore % 2))
+			return cursore - 2;
 		else
 			return cursore;//чтобы не шло выше
 		break;
 	case ENTER:
-		if (cursore < SIZE)
+		if (!(cursore % 2))
 		{
-			if (cursore == SIZE-1)
+			if (cursore == 2*SIZE - 2)
 			{
 				menu = false;
 				return 0;
 			}
 			else
-				return  cursore + SIZE;
+				return  cursore + 1;
 		}
-		else 
-			return cursore - SIZE ;
+		else
+			return cursore - 1;
 		break;
 	default:
 		return cursore;
 	}
 }
-
+void add(string*& arr, int & size, const string point, const string text)
+{
+	string* new_Array = new string[2*size + 2];
+	for (int i = 0; i < 2*size-2; i++)
+		new_Array[i] = arr[i];
+	new_Array[2*size-2] = point;
+	new_Array[2*size-1] = text;
+	new_Array[2 * size]=arr[2*size-2];
+	new_Array[2*size+1]=arr[2*size-1];
+	delete[] arr;
+	arr = new_Array;
+	size++;
+}
 
 int main()
 {
@@ -78,18 +89,13 @@ int main()
 	bool menu = true;
 	setlocale(LC_ALL, "Rus");
 	int cursore = 0;
-	string array[2*SIZE];
-	array[0] = "Пункт 1";
-	array[1] = "Пункт 2";
-	array[2] = "Пункт 3";
-	array[3] = "Пункт 4";
-	array[4] = "Выход";
-
-	array[5] = " Вы вошли в Пункт 1";
-	array[6] = " Вы вошли в Пункт 2";
-	array[7] = " Вы вошли в Пункт 3";
-	array[8] = " Вы вошли в Пункт 4";
-	array[9] = " Нажмите Enter чтобы выйти";
+	int SIZE = 1;
+	string* array = new string[2 * SIZE];
+	array[0] = "выход";
+	array[1] = " нажмите enter чтобы выйти";
+	add(array, SIZE, "1point", "1text");
+	add(array, SIZE, "2point", "2text");
+	add(array, SIZE, "3point", "3text");
 
 	while (menu)
 	{
@@ -97,9 +103,9 @@ int main()
 		SetConsoleTextAttribute(hCon, (WORD)((0 << 4) | 12));
 		Draw();
 		SetConsoleTextAttribute(hCon, (WORD)((0 << 4) | 15));
-	if (cursore < SIZE)
+	if (!(cursore % 2))
 	{
-		for (int i = 0; i < SIZE; i++)
+		for (int i = 0; i < 2*SIZE; i+=2)
 		{
 			cPos.Y = 3 + i;
 			SetConsoleCursorPosition(hCon, cPos);//устанавливает курсор на поставленные координаты
@@ -122,6 +128,6 @@ int main()
 			SetConsoleCursorPosition(hCon, cPos);
 			cout << array[2*SIZE-1];
 		}
-		cursore = Key(cursore, menu);
+		cursore = Key(cursore, menu,SIZE);
 	}
 }
